49d5344620f6b2a623f19f1ca8f310995f2440cf
mishra <jisumishra4@gmail.com>
2026-04-15 02:25:15 +0530

    Creating a route for the server

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+// this is create instance of the express.js application
+const app = express();
+
+app.use("/hellow",(req,res) =>{
+  res.send("Hellow, Hellow, Hellow ........ ?");
+})
+
+app.use("/test",(req,res) =>{
+  res.send("Hellow From The Server Testing");
+})
+
+app.use("/",(req,res) =>{
+  res.send("Hellow From The Server");
+})
+
+app.listen(3000, () => {
+  console.log("Server is SuccesFully Listening On Port 3000");
+});
