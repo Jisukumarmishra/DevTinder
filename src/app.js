e5c113678a065321e9facbbf1cb5f83bc38ea9dd
mishra <jisumishra4@gmail.com>
2026-04-16 19:49:44 +0530

    Write The Api call for get, post, patchdelete and test at the postman

--- src/app.js
+++ src/app.js
@@ -3,9 +3,33 @@ const express = require('express');
 // this is create instance of the express.js application
 const app = express();
 
+// app.use("/user", (req, res)=> {
+//   res.send("HAAAAAAAAAA.......");
+// });    
+
+// this will only handle get call to /user
+app.get("/user", (req, res) =>{
+  res.send({firstname:"Jisu", lastname: "Mishra"});
+});
+
+app.post("/user", (req, res) => {
+// write the logic for the  saving data to db
+res.send("Data successfully saved to the database");
+});
+
+app.patch("/user", (req,res) => { 
+res.send("User updated successfully For The Patch Route");
+});
+
+app.delete("/user", (req,res) => {
+res.send("Deleted Succesfully");
+});
+
+
+// this route will match all the http method API calls to /test
 app.use("/test",(req,res) =>{
-  res.send("Hellow From The Server Testing");
-})
+  res.send("Hellow From The Server Testing Team Hi there ");
+});
 
 app.use("/hellow",(req,res) =>{
   res.send("Hellow, Hellow, Hellow ........ ?");
