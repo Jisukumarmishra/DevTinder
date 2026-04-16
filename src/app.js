31d266e8355dd202233dfb72dcbc6f91c2120a89
mishra <jisumishra4@gmail.com>
2026-04-17 00:09:41 +0530

    Explore The use Of ? , + ,(), * in routes also regex in route like /a/

--- src/app.js
+++ src/app.js
@@ -3,27 +3,33 @@ const express = require('express');
 // this is create instance of the express.js application
 const app = express();
 
+
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send({firstname: "Jisu", lastname: "mishra keep Your prioprities First"});
+});
+
 // app.use("/user", (req, res)=> {
 //   res.send("HAAAAAAAAAA.......");
 // });    
 
 // this will only handle get call to /user
-app.get("/user", (req, res) =>{
-  res.send({firstname:"Jisu", lastname: "Mishra"});
-});
+// app.get("/user", (req, res) =>{
+//   res.send({firstname:"Jisu", lastname: "Mishra"});
+// });
 
-app.post("/user", (req, res) => {
-// write the logic for the  saving data to db
-res.send("Data successfully saved to the database");
-});
+// app.post("/user", (req, res) => {
+// // write the logic for the  saving data to db
+// res.send("Data successfully saved to the database");
+// });
 
-app.patch("/user", (req,res) => { 
-res.send("User updated successfully For The Patch Route");
-});
+// app.patch("/user", (req,res) => { 
+// res.send("User updated successfully For The Patch Route");
+// });
 
-app.delete("/user", (req,res) => {
-res.send("Deleted Succesfully");
-});
+// app.delete("/user", (req,res) => {
+// res.send("Deleted Succesfully");
+// });
 
 
 // this route will match all the http method API calls to /test
