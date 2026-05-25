61a5c3ed63e9cb252c7242040f21959d22187504
mishra <jisumishra4@gmail.com>
2026-05-25 15:07:44 +0530

    Making SignUp Page Is Dynamic to receive data From The end User

--- src/app.js
+++ src/app.js
@@ -7,29 +7,19 @@ const app = express() // instaces of express
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  console.log(req.body); 
 // creating the new instaces of the user model
-// const user =  new User({
-//   firstName : "Jisu",
-//   lastName : "Mishra",
-//   emailId : "jisuk138@gmail.com",
-//   password : "jisuuu@123"
-// });
+const user =  new User(req.body);
 
-//   const user = new User ({
-//     firstName : "Harsh",
-//     lastName  : "Mishra",
-//     emailId : "harsh@gmail.com",
-//     passWord : "Harsh@123"
-//   })
+console.log(req.body);
 
-// try {
-//  await user.save();
-//  res.send("user addeded succesfully ");
-// }
-// catch (err) {
-//   res.status(400).send("Error saving the user :"+ err.message);
-// }
+try {
+ await user.save();
+ res.send("user addeded succesfully ");
+}
+catch (err) {
+  res.status(400).send("Error saving the user :"+ err.message);
+}
+
 });
 
 
