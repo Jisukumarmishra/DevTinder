f9bc8da3fc0c0d21f9de5883dd4796ef069ce41a
mishra <jisumishra4@gmail.com>
2026-05-27 14:43:02 +0530

    Writing THe UserId USing THE HElp Of THe mOngoose Documentation ANd Also Keep The Habit Of hte Reading The Documents

--- src/app.js
+++ src/app.js
@@ -94,6 +94,16 @@ app.get("/:id", async (req,res) => { //"/user/:id"(for params -> :)
   }
 });
 
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId; // read the body
+  try {
+  // const user = await User.findByIdAndUpdate( {_id : userId })
+  const user = await User.findByIdAndDelete(userId)
+  } catch (err) {
+    req.status(400).send("Something Went Wrong")
+  }
+})
+
 
 
 
