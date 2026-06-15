f0e3c477776d56fbce8ab781f4706e936f29776f
mishra <jisumishra4@gmail.com>
2026-06-15 13:26:38 +0530

    Adding user connection send api

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { userAuth } = require('../middlewares/auth');
+const ConnectionRequest = require('../models/connectionRequest');
 const requestRouter = express.Router();
 
 requestRouter.post("/sendConnectionRequest", userAuth, async(req, res) => {
@@ -8,5 +9,33 @@ requestRouter.post("/sendConnectionRequest", userAuth, async(req, res) => {
   console.log("Sending a Connections Request");
   res.send(user.firstName +  "sent the connection request");
 });
+
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
+  try {
+  const fromUserId = req.user._id;
+  const toUserId = req.params.toUserId;
+  const status = req.params.status;
+
+ 
+  //creating a instances of the ConnectionRequest model
+  const connectionRequest = new ConnectionRequest({
+   fromUserId,
+   toUserId,
+   status
+  });
+
+  // save connectionRequestData in the database
+  const data = await connectionRequest.save();
+
+  res.json({
+  message : "Connection Request Send Succeffuly ",
+  data,
+  });
+
+  } 
+  catch (err) {
+ res.status(400).send("ERROR : " + err.message);
+  }
+})
 
 module.exports = requestRouter;
