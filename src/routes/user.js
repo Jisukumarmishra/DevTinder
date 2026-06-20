63f9ff3309113cb7b4256b7bc985a632509faca8
mishra <jisumishra4@gmail.com>
2026-06-20 16:59:14 +0530

    Adding The Logic Of Request Received Api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,27 @@
 const express = require('express');
+const { userAuth } = require('../middlewares/auth');
+const ConnectionRequestModel = require('../models/connectionRequest');
 const userRouter = express.Router();
+
+// get the all pending connections requests for the loggedInUser
+userRouter.get("/user/requests/received", userAuth, async (req, res ) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequestModel.find({
+      toUserId : loggedInUser._id,
+      // status pending
+      status : "interested"
+    })
+
+    res.json({
+      message : "data fetched successfully",
+      data : connectionRequests,
+    })
+
+  }
+  catch (err) {
+    res.status(400).send("ERROR : " + err.message );
+  }
+});
 
 module.exports = userRouter;
